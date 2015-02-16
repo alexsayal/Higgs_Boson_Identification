46b9eb72e2b0230ef5e36d33a76e33f4ac2782d3
Alexandre Sayal <alexandresayal@gmail.com>
2015-02-16 23:48:35 +0000

    Experimentando...

--- Aula2/AS/fruits.m
+++ Aula2/AS/fruits.m
@@ -20,15 +20,44 @@ for i=1:length(apples)
    green = A(window_c,window_l,2);
    blue = A(window_c,window_l,3);
    
+   M_red = [min(min(red)) max(max(red))];
+   M_green = [min(min(green)) max(max(green))];
+   M_blue = [min(min(blue)) max(max(blue))];
+   
    subplot(1,4,1); imhist(red); title('Red Channel');
    subplot(1,4,2); imhist(green); title('Green Channel');
    subplot(1,4,3); imhist(blue); title('Blue Channel');
 
    A_bw = rgb2gray(A);
-   A_bw2 = im2bw(A_bw);
-   subplot(1,4,4); imshow(A_bw2); 
+   B = decorrstretch(A_bw);
+   %A_bw = A;
+   
+   B = imsharpen(A_bw);
+   B = imadjust(B);
+   B = im2bw(B);
+   subplot(1,4,4); imshow(B);
+   
+%    threshold = graythresh(A_bw);
+%    A_bw2 = im2bw(A_bw,threshold);
+%    se = strel('disk',2);
+%    A_bw2 = imclose(A_bw2,se);
+%    A_bw2 = imfill(A_bw2,'holes');
+%    
+%    [B,L] = bwboundaries(A_bw2,'noholes');
+% 
+%    subplot(1,4,4); imshow(label2rgb(L, @jet, [.5 .5 .5]))
+%    hold on
+%    for k = 1:length(B)
+%        boundary = B{k};
+%        subplot(1,4,4); plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+%    end
+   
+   %A_bw2 = im2bw(A_bw);
+   %subplot(1,4,4); imshow(A_bw2); 
+   %subplot(1,4,4); imcontour(A_bw,3); 
    
    pause;
+   hold off
 end
 
 %% Peaches
