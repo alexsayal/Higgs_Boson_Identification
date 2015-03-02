94720197c732a725216af24c32872e0bc524cc9b
Alexandre Sayal <alexandresayal@gmail.com>
2015-03-02 19:50:43 +0000

    Aula 2 Mar

---
+++ Aula4/Exer2.m
@@ -0,0 +1,49 @@
+%%%%% = Técnicas de Reconhecimento de Padrões = %%%%%
+%%%%% ================= Aula 4 ================ %%%%%
+%%%%% ================== 2015 ================= %%%%%
+
+%% Load
+[xls_data,col_names]=xlsread('CORK_STOPPERS.XLS','Data');
+
+%% Preprocessing and prep for STPR
+data_raw = xls_data(:,3:end);
+
+data.X = scalestd(data_raw)';
+data.y=xls_data(:,2)';
+
+%% Simpler problem
+subset1 = data.X([1,3],1:50);
+
+coeff1 = pca(subset1); %PCA
+
+I = coeff1.W * coeff1.W' %Prove it is orthogonal
+
+c = coeff1.eigval(1)*100/sum(coeff1.eigval) %Contribution of the 1st PC
+
+% Graph 1
+figure(1);
+scatter(subset1(1,:),subset1(2,:)); title('Sem PCA'); xlabel('ART'); ylabel('PRT');
+
+% Graph 2
+subset2 = linproj(subset1,coeff1);
+figure(2);
+scatter(subset2(1,:),subset2(2,:)); title('Com PCA'); xlabel('PC 1'); ylabel('PC 2');
+
+%% For the complete dataset
+
+coeff2 = pca(data.X); %PCA
+s = coeff2.eigval
+
+subset3.X = linproj(data.X(:,1:100),coeff2);
+subset3.y = xls_data(1:100,2)';
+subset3.dim = size(data.X(:,1:100),1);
+subset3.num_data=size(data.X(:,1:100),2);
+
+figure(3);
+ppatterns(subset3); title('Com PCA'); xlabel('PC 1'); ylabel('PC 2');
+
+figure(4);
+plot(s,'--o'); title('Valores próprios');
+
+PC_kaiser = length(s(s>1))
+PC_scree = length(s(s>0.01))
