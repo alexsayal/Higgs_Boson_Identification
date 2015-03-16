1b21c817d8941b8830d85c0b63a58d2121e92cfc
Sara Pires Oliveira <sara.pires.oliveira@hotmail.com>
2015-03-16 20:21:49 +0000

    RECOMITE

--- Aula6/Exer2.m
+++ Aula6/Exer2.m
@@ -2,3 +2,23 @@
 %%%%% ================= Aula 6 ================ %%%%%
 %%%%% ================== 2015 ================= %%%%%
 
+[xls_data,col_names]=xlsread('BREAST_TISSUE.XLS','Data');
+
+%% Structure
+
+data.X = xls_data(1:106,:)';
+data.y = col_names(2:107,1)';
+data.dim = size(data.X,2);
+data.num_data = size(data.X,1);
+
+data.y=strcmp('gla',col_names(2:107,1)) + strcmp('adi',col_names(2:107,1)) + strcmp('con',col_names(2:107,1));
+data.y(data.y==0) = 2;
+
+data.X = scalestd(data.X);
+
+%% hist e hist_fit
+
+figure()
+hist(data.X);
+histfit(data.X);
+
