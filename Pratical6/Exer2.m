12b2c494b6122177ae8e33801245b877c0fa66f6
Alexandre Sayal <alexandresayal@gmail.com>
2016-10-28 17:12:48 +0100

    Global Reorganisation

--- Pratical6/Exer2.m
+++
@@ -1,7 +0,0 @@
-dist = xlsread('prob1dist.xls');
-
-Z1 = linkage(dist,'single');
-Z2 = linkage(dist,'complete');
-
-figure(); dendrogram(Z1); title('Single');
-figure(); dendrogram(Z2); title('Complete');
